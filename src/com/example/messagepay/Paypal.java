2e942c379bf3f9e709536dbd8a7cb7cce2e0a5c6
siyliu <siyliu@L-SJN-00554636.corp.ebay.com>
2014-12-01 22:11:14 -0800

    Facebook login

---
+++ src/com/example/messagepay/Paypal.java
@@ -0,0 +1,61 @@
+package com.example.messagepay;
+
+import com.facebook.AppEventsLogger;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Paypal extends FragmentActivity {
+	
+	private MainFragment mainFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new MainFragment();
+            getSupportFragmentManager().beginTransaction().add(android.R.id.content, mainFragment)
+            .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (MainFragment) getSupportFragmentManager()
+            .findFragmentById(android.R.id.content);
+        }
+    }
+    
+    @Override
+    protected void onResume(){
+    	super.onResume();
+    	AppEventsLogger.activateApp(this);
+    }
+    
+    @Override
+    protected void onPause(){
+    	super.onPause();
+    	AppEventsLogger.deactivateApp(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.paypal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
